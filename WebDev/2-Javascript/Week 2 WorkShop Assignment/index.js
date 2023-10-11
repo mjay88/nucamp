bbc129bdb7be5bf64b3744c30444d8dee1a8b0a4
Michael Jarrett <mwilliamkjarrett@gmail.com>
2023-10-10 21:21:31 -0500

    start week 2 workshop

---
+++ WebDev/2-Javascript/Week 2 WorkShop Assignment/index.js
@@ -0,0 +1,50 @@
+const COLORS_ARRAY = [
+	"blue",
+	"cyan",
+	"gold",
+	"gray",
+	"green",
+	"magenta",
+	"orange",
+	"red",
+	"white",
+	"yellow",
+];
+
+function runGame() {
+	let guess = "";
+	let correct = false;
+	const targetIndex = Math.floor(Math.random() * COLORS_ARRAY.length);
+	const target = COLORS_ARRAY[targetIndex];
+ let count = 0;
+	do {
+		guess = prompt(`I am thinking of one of these colors: \n\n 
+     ${COLORS_ARRAY.join(", ")}\n\n
+     What color am I thinking of? \n
+    `);
+		if (guess === null) {
+			alert("You didn't enter anything");
+			return;
+		}
+		correct = checkGuess(guess, target);
+        count++
+	} while (!correct);
+
+    alert(`Congratulations, you guessed correctly in ${count} tries`)
+    
+}
+
+function checkGuess(guess, target) {
+ let correct = false;
+ if(!COLORS_ARRAY.includes(guess)){
+    alert("That is not a valid color")
+ } else if(COLORS_ARRAY.indexOf(guess) > COLORS_ARRAY.indexOf(target)){
+    alert("You were close, but a little too high...")
+ } else if(COLORS_ARRAY.indexOf(guess) < COLORS_ARRAY.indexOf(target)){
+    alert("You were close, but a little too low...");
+ } else {
+    correct = true;
+ }
+
+ return correct;
+}
