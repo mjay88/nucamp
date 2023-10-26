ba04c397423b3db07cc46460b84d71627bf357af
mjay88 <mwilliamkjarrett@gmail.com>
2023-10-25 21:52:20 -0500

    working on game logic, stopPropigation

--- WebDev/2-Javascript/Week-4/matching-game/index.js
+++ WebDev/2-Javascript/Week-4/matching-game/index.js
@@ -2,7 +2,12 @@
 let numberOfFaces = 5;
 const theLeftSide = document.querySelector("#leftSide");
 const theRightSide = document.querySelector("#rightSide");
+const container = document.querySelector(".container")
 let topOfLeft = theLeftSide.getBoundingClientRect();
+const topContainerNode = document.querySelector(".top-container");
+
+
+let streak = 0;
 console.log(topOfLeft);
 //generate faces function
 function generateFaces() {
@@ -21,19 +26,20 @@ function generateFaces() {
     leftSideImagesClone.removeChild(leftSideImagesClone.lastChild);
     while(leftSideImagesClone.firstChild){
 		let image = leftSideImagesClone.firstChild;
-        console.log(image.style.left, "image style before");
+        // console.log(image.style.left, "image style before");
         
         image.style.left =
 		Number(image.style.left.slice(0, -2)) +
 		theRightSide.getBoundingClientRect().left +
 		"px";
         // console.log((Number(image.style.left.slice(0, -2)) + theRightSide.getBoundingClientRect().left) , 'image style')
-		console.log(image.style.left, 'after')
+		// console.log(image.style.left, 'after')
         theRightSide.appendChild(image);
     }
 	let missingFace = theLeftSide.lastChild;
 	missingFace.addEventListener('click', nextLevel)
-	document.body.addEventListener('click', gameOver);
+	container.addEventListener('click', gameOver);
+
 }
 
 function nextLevel(e){
@@ -43,11 +49,53 @@ console.log("clicked")
 numberOfFaces += 1;
 theLeftSide.innerHTML = "";
 theRightSide.innerHTML = "";
+appendStreak();
 generateFaces();
 }
 
-function gameOver() {
+function gameOver(e) {
+	e.stopPropagation();
+
+	console.log('game over firing')
 	alert('Game over, you lose');
-	theLeftSide.lastChild.removeEventListener("click", nextLevel);
-	document.body.removeEventListener("click", gameOver);
+	// theLeftSide.lastChild.removeEventListener("click", nextLevel);
+	container.removeEventListener("click", gameOver);
+	topContainerNode.removeEventListener("click", gameOver)
+	const button = document.createElement("button");
+	button.classList.add("restart-button");
+	button.textContent = "Restart Game"
+	button.removeEventListener("click", gameOver);
+	button.addEventListener('click', restart)
+	topContainerNode.appendChild(button);
+
+}
+
+function appendStreak(){
+	
+	streak++
+	const topContainerNode = document.querySelector('.top-container');
+	topContainerNode.removeChild(topContainerNode.lastChild)
+	let streakNode = document.createElement("span");
+
+	streakNode.textContent = `Your winning streak is: ${streak}`;
+	// console.log(streakNode);
+	// console.log(topContainerNode);
+
+	topContainerNode.appendChild(streakNode);
+
+}
+
+function restart(e){
+	e.stopPropagation(e);
+	container.removeEventListener("click", gameOver);
+	//empty left and right 
+	theRightSide.innerHTML = "";
+	theLeftSide.innerHTML = "";
+	streak = 0;
+	//remove button
+	let button = document.querySelector(".restart-button")
+    console.log(e.target, 'e.taraget.value for restart')
+	console.log(button, 'button should be removed')
+	topContainerNode.removeChild(button)
+	generateFaces()
 }
