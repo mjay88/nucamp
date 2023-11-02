2bf6e5a5e068bf1c50d9b1652cc1951371475eae
Michael Jarrett <mwilliamkjarrett@gmail.com>
2023-11-02 15:03:06 -0500

    add media query for small phones for reset button

--- WebDev/2-Javascript/tic-tac-toe/index.js
+++ WebDev/2-Javascript/tic-tac-toe/index.js
@@ -20,30 +20,28 @@ squares.forEach((square, i) => {
 // resetButton.addEventListener('click', restartGame)
 
 function makeMove(square, row, col) {
-	square.innerHTML =
-		currentPlayer % 2 === 0 ? "<i class='fa fa-o'>" : "<i class='fa fa-x'>";
-    numberOfMoves++
-    square.disabled = true;
-    //set the board
-    board[row][col] = currentPlayer;
-    if(didPlayerWin()){
-        gameHeading.textContent = `Player ${currentPlayer} is the Winner`
-        endGame();
-    } else if(numberOfMoves >= BOARD_WIDTH * BOARD_WIDTH){
-        gameHeading.textContent = "Tie Game!";
-        endGame();
-    } else {
-        currentPlayer = currentPlayer === 1 ? 2 : 1;
-        setCurrentPlayerHeader();
-    }
-
+	square.innerHTML = currentPlayer % 2 === 0 ? "X" : "O";
+	numberOfMoves++;
+	square.disabled = true;
+	//set the board
+	board[row][col] = currentPlayer;
+	if (didPlayerWin()) {
+		gameHeading.textContent = `Player ${currentPlayer} is the Winner`;
+		endGame();
+	} else if (numberOfMoves >= BOARD_WIDTH * BOARD_WIDTH) {
+		gameHeading.textContent = "Tie Game!";
+		endGame();
+	} else {
+		currentPlayer = currentPlayer === 1 ? 2 : 1;
+		setCurrentPlayerHeader();
+	}
 }
 
 function didPlayerWin() {
 	const rows = [0, 1, 2];
 
 	const wonHorizontal = rows.some((row) => {
-		return (    
+		return (
 			board[row][0] === currentPlayer &&
 			board[row][1] === currentPlayer &&
 			board[row][2] === currentPlayer
@@ -77,50 +75,36 @@ function didPlayerWin() {
 	);
 }
 
-function endGame(){
-    let resetButton = document.createElement("button")
-    resetButton.classList.add("restart-button", "btn", "btn-secondary");
-    resetButton.textContent = "Restart";
-    resetButton.addEventListener('click', restartGame);
-    document.querySelector(".container").appendChild(resetButton);
+function endGame() {
+	let resetButton = document.createElement("button");
+	resetButton.classList.add("restart-button", "btn", "btn-secondary");
+	resetButton.textContent = "Restart";
+	resetButton.addEventListener("click", restartGame);
+	document.querySelector(".container").appendChild(resetButton);
 
-    // resetButton.classList.remove("d-none");
-    squares.forEach((square) => {
-        square.disabled = true;
-    })
+	// resetButton.classList.remove("d-none");
+	squares.forEach((square) => {
+		square.disabled = true;
+	});
 }
 
-function setCurrentPlayerHeader(){
-    gameHeading.textContent = `Player ${currentPlayer}'s turn`
+function setCurrentPlayerHeader() {
+	gameHeading.textContent = `Player ${currentPlayer}'s turn`;
 }
 
-function restartGame(){
-    board = generateEmptyboard();
-    currentPlayer = 1;
-    numberOfMoves = 0;
-    squares.forEach((square) => {
-        square.innerHTML = "";
-        square.disabled = false;
-    });
-    setCurrentPlayerHeader();
-    // resetButton.classList.add("d-none")
-    let container = document.querySelector(".container");
-    container.removeChild(container.lastChild);
+function restartGame() {
+	board = generateEmptyboard();
+	currentPlayer = 1;
+	numberOfMoves = 0;
+	squares.forEach((square) => {
+		square.innerHTML = "";
+		square.disabled = false;
+	});
+	setCurrentPlayerHeader();
+	let container = document.querySelector(".container");
+	container.removeChild(container.lastChild);
 }
 
-function generateEmptyboard(){
-    return new Array(BOARD_WIDTH).fill().map(() => new Array(BOARD_WIDTH).fill());
+function generateEmptyboard() {
+	return new Array(BOARD_WIDTH).fill().map(() => new Array(BOARD_WIDTH).fill());
 }
-
-//GraveYard
-    // if(currentPlayer % 2 === 0){
-    //  let player1Move = document.createElement('<i>');
-    //  player1Move.classList.add('fa')
-    //  player1Move.classList.add('fa-o')
-    //     square.appendChild(player1Move);
-    // } else {
-    // let player2Move = document.createElement("<i>");
-    // player2Move.classList.add("fa");
-    // player2Move.classList.add("fa-x");
-    // square.appendChild(player2Move);
-    // }
